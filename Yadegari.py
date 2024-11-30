cbb136520bdc6c398378932c92336f18f60f475f
DanieLRysoN <hossein138778toreihi@gmail.com>
2024-12-01 00:37:56 +0330

    Add files via upload

---
+++ Yadegari.py
@@ -0,0 +1,12 @@
+import os
+import time
+import requests
+from threading import Thread
+import colorama
+import os
+proxy = {"https": "127.0.0.1.8000"}
+os.system("clear")
+print(colorama.Fore.CYAN+'MAXTOR')
+time.sleep(2)
+os.system('clear')
+print(colorama.Fore.RED+""'
