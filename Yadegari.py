4af48b7ba85b857d68075fc2dcd29a748274ae69
DanieLRysoN <hossein138778toreihi@gmail.com>
2024-12-01 14:45:45 +0330

    Update Yadegari.py

--- Yadegari.py
+++ Yadegari.py
@@ -26,7 +26,7 @@ print(f"{green} ")
 print (Fore.WHITE + "")
 
 x = str(datetime.datetime.now())
-print("(x))
+print("x")
 print("SCRIPT YADEGARI....")
 print("(...)DanieL RysoN...)")
 print("Bemand Yadegri Sal 1403 or 2024")
