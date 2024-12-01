922c2914566032a2203548fb977da65934203b1e
DanieLRysoN <hossein138778toreihi@gmail.com>
2024-12-01 14:44:33 +0330

    Update Yadegari.py

--- Yadegari.py
+++ Yadegari.py
@@ -1,16 +1,46 @@
 #python 2.7.15
-# GoD FilterinG
+print("Load . . .")
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# rang
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+x = str(datetime.datetime.now())
+print("(x))
 print("SCRIPT YADEGARI....")
-print("(...DanieL RysoN...)")
+print("(...)DanieL RysoN...)")
 print("Bemand Yadegri Sal 1403 or 2024")
-print("")
-print("")
-print(" ")
-print("""
+
+print"███          █       ██    █
+█    █     █   █     █ █   █
+█     █   █     █    █  █  █
+█     █  █▀▀▀▀▀▀█   ‌█   █ █
+█    █  █            █  
+███    █          █ █"
+
 ███          █       ██    █
 █    █     █   █     █ █   █
 █     █   █     █    █  █  █
 █     █  █▀▀▀▀▀▀█   ‌█   █ █
 █    █  █         █  █  
-███    █          █ █""")
-print(" ")
+███    █          █ █
