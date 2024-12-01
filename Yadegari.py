5a6adfd9630005b773aec13bc7b5a3c07c5d6789
DanieLRysoN <hossein138778toreihi@gmail.com>
2024-12-01 14:24:32 +0330

    Add files via upload

---
+++ Yadegari.py
@@ -0,0 +1,14 @@
+#python 2.7.15
+
+print "SCRIPT YADEGARI...."
+print "(...)DanieL RysoN...)"
+print "Bemand Yadegri Sal 1403 or 2024"
+
+print " 
+
+███          █       ██    █
+█    █     █   █     █ █   █
+█     █   █     █    █  █  █
+█     █  █▀▀▀▀▀▀█   ‌█   █ █
+█    █  █            █  
+███    █          █ █"
