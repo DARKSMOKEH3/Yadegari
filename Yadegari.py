f49cafe55db599386b4b81080e7f4f501a48e3f4
DanieL RysoN <hossein138778toreihi@gmail.com>
2024-12-03 00:58:38 +0330

    Update Yadegari.py

--- Yadegari.py
+++ Yadegari.py
@@ -2,13 +2,8 @@
 import os
 os.system("pip install colored")
 os.system("pip install colorama")
-os.system("pip install datetime")
-#importing
-import time                                                     
-import sys                                                      
 import colored                                                  
 import colorama                                                 
-import datetime
 from colored import fg, bg, attr
 from colorama import Fore, Back, Style                          
 # rang
@@ -17,181 +12,28 @@ green='\033[32m'
 blue='\033[36m'
 pink='\033[35m'
 rang='\033[34m'
-
-
 os.system("clear")
-
 print(f"{green} ")
-
-print (Fore.WHITE + "")
+print("In ThE NaMe Of GoD")
 print(f"{green} ")
-print (Fore.GREEN + "")
-x = """ 
-                ***In The Name Of MamaD RysoN***"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-print("                God filtering and Cyberi __MAMAD RYSON__")
-print("***************************************************************")
-print(f"{blue} ")
-print("....SCRIPT YADEGARI....")
+print("GoD KLR * Shad , Rubika")
+print(f"{green} ")
+print("DaLtOn ErRoR,ShaHzAdeH TiKaFiL")
+print(f"{green} ")
+print("          *          ")
+print(f"{green} ")
+print("The BeSt KLR HoSeIn DaNiEL RySoN")
 print(f"{red} ")
-print("           GoD klr")
+print("Bemanad Yadegar Shayad Fardayi Nabashad")
 print(f"{red} ")
-print("          i Am")
-print(f"{blue} ")
-print (Fore.BLUE + "")
-x = """ 
-                         (...DanieL RysoN...)"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-print("                    ***Khalegh BaX KaizofiL and khalegh leonard &....***")
-print(f"{green} ")
-print("      Bemand Yadegri Sal 1403 or 2024")
-print (Fore.BLUE + "")
-x = """ °•°•°•°•°•°•°•°•°•°•°
-     
-                   ⣿⣿⣿⠻   ⣿⣿     ⣿⣿   ⣿  ⣿  ⣿⣿⣿⣿   ⣿
-                   ⣿   ⣿  ⣿ ⣿    ⣿ ⣿  ⣿  ⣿  ⣿      ⣿
-                   ⣿   ⣿  ⣿  ⣿   ⣿  ⣿ ⣿  ⣿  ⣿⣿⣿⣿   ⣿
-                   ⣿   ⣿  ⣿⣿⣿⣿⣿  ⣿   ⣿⣿  ⣿  ⣿      ⣿
-                   ⣿⣿⣿⣼   ⣿    ⣿ ⣿    ⣿  ⣿  ⣿⣿⣿⣿   ⣿⣿⣿⣿⣿
-              
-"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.02)
-
-print (Fore.RED + "")
-x = """
-          ⣿⣿⣿⣿    ⣿    ⣿   ⣿⣿⣿⣿    ⣿⣿⣿⣿   ⣿⣿   ⣿       
-  ⣸⣇      ⣿   ⣿    ⣿   ⣿   ⣿       ⣿  ⣿   ⣿ ⣿  ⣿
-⠙⢿⣿⣿⡿⠋    ⣿⣿⣿⣿⣿     ⣿ ⣿    ⣿⣿⣿⣿    ⣿  ⣿   ⣿  ⣿ ⣿
-⠀⣼⠟⠻⣧     ⣿   ⣿     ⣿         ⣿    ⣿  ⣿   ⣿   ⣿⣿
-⠀⠀⠀⠀⠀⠀    ⣿   ⣿    ⣿       ⣿⣿⣿⣿    ⣿⣿⣿⣿   ⣿    ⣿
-"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.02)
-print (Fore.GREEN + "")
-x = """
-                    Be Name Bozorgane refaghat: """
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.2)
-
-
+print("1403 - 2024")
+print(f"{red} ")
+print("     =~=~=~=~=~=~=~=~=~=~=     ")
 print (Fore.BLUE + "")
 x = """
-                    ***\\....MamaD RysoN....//***       """
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.BLUE + "")
-x = """
-                      **....MmD JenA....**     """
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.GREEN + "")
-x = """ 
-                       **....WalteR AlfadeR**"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
- 
-     
-print (Fore.GREEN + "")
-x = """ 
-                         *....MmD TerroR....*"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.RED + "")
-x = """ 
-                         ....YasiN AlonE...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.RED + "")
-x = """ 
-                         ....MamaD Entehari...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.YELLOW + "")
-x = """ 
-                         ....MeysaM TokiyO...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.YELLOW + "")
-x = """ 
-                          ....MG KhataR...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-print (Fore.BLUE + "")
-x = """ 
-                         ....Gost Black Report...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-
-
-print (Fore.RED + "")
-x = """ 
-                          ....Bax KaizofiL va Baghie Doostan...."""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.05)
-print (Fore.YELLOW + "")
-time.sleep(1.7)
-print ("           TELEGRAM ID -  @U79_9   ")
-print ("                         telegram Channel : @Daniel_ryson")
-print (Fore.BLUE + "")
-x = """ 
-            filteri tah nadare
-            Be EdalaT raftaR kon
-            BashaD YadegaRi az HOSSEIN TORI YA DANIEL RYSON... 
-            YA HAGH"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
- 
-up = input(""" 1 Ro bezan""")
-print("")
-print(f"{pink} ")
-time.sleep(1.0)
-print()
-if up == "1":
-    os.system("exit 5")
+*Taqdim : Bax Carter , Makan Tomar , Bax Mordegan , Bax Tomar , Beni Carter , Daniel Rayson , Bax Begacifer , Mmd Jena , Allah Tikafil , DaLtoN ErRor , Mr Mmd Reporter*
+     ~Record To Filtering : Fil account 'rubika' 7 Ellie 3 Min~
+ •I'M ' 17 Age•
+           *   
+       .<script.Yadegari>.
+     ∅I live in the city of Tabriz∅ """
